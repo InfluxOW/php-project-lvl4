392783a523d275e20b155e0535d6e8ebed09d657
InfluxOW <krochak_n@mail.ru>
2020-04-17 21:34:56 +1000

    remove semantic ui (its in another branch now)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,6 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
-    {{-- Semantic UI CSS --}}
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/semantic.min.css') }}">
 
     <title>Task Manager</title>
   </head>
@@ -73,8 +71,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
 <script src="//code.jquery.com/jquery.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-{{-- Semantic UI JavaScript --}}
-<script src="{{ asset('js/semantic.min.js') }}"></script>
 
 <script>
     $('div.alert').not('.alert-important').delay(2000).fadeOut(2000);
